09d0829f9bcc77a2797c5fc1461f0067b02f14a8
Rinni Swift <rinradaswift@gmail.com>
2018-08-14 11:06:47 -0700

    note title and modification time displayed in table view.

---
+++ RinniNotes/RinniNotes/Controller/ListNotesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListNotesTableViewCell.swift
+//  RinniNotes
+//
+//  Created by Rinni Swift on 8/14/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var noteTitleLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    
+}
---
+++ RinniNotes/RinniNotes/Model/DisplayNotesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DisplayNotesViewController.swift
+//  RinniNotes
+//
+//  Created by Rinni Swift on 8/13/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DisplayNotesViewController: UIViewController {
+    
+    
+}
